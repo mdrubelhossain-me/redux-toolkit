d55b5829ee8c6327f32a781e20badc07270f314a
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-05-06 17:30:17 +0600

    counter app complete

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+// স্টেটের টাইপ ডিফাইন করো
+interface RootState {
+  count: number;
+}
 
 const Counter: React.FC = () => {
+  // এখানে টাইপ স্পষ্ট করে বলা হয়েছে
+  const count = useSelector((state: RootState) => state.count);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({
+      type: "INCREMENT",
+    });
+  };
+
+  const handleDecrement = () => {
+    dispatch({
+      type: "DECREMENT",
+    });
+  };
+
+  const handleReset = () => {
+    dispatch({ type: "RESET" });
+  };
+
   return (
     <div>
-      <h1>Counter: 0</h1>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <h1>Counter: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 };
